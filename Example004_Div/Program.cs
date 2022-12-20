72e987d1e3811f1e705551120441f72fa9c6f5c6
--replace-all <zinovskayak@gmail.com>
2022-12-20 23:48:16 +0100

    Example004_Div

---
+++ Example004_Div/Program.cs
@@ -0,0 +1,6 @@
+﻿int numberA = 42;
+int numberB = 5;
+//можно сделать с промежуточной переменной:
+//int result = numberA + numberB;
+//Console.WriteLine(result);
+Console.WriteLine(numberA + numberB);
