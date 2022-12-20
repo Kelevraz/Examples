1826d2e15064495711eb34f5e377b20a631e2357
--replace-all <zinovskayak@gmail.com>
2022-12-20 23:39:28 +0100

    Example003_Sum

---
+++ Example003_Sum/Program.cs
@@ -0,0 +1,9 @@
+﻿// вывод суммы двух чисел
+//numberA - переменная
+
+int numberA = 42;
+int numberB = 5;
+//можно сделать с промежуточной переменной:
+//int result = numberA + numberB;
+//Console.WriteLine(result);
+Console.WriteLine(numberA + numberB);
