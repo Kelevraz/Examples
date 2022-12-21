dc4b99d3592ac1eb4679772328fd445bc0e87a3f
--replace-all <zinovskayak@gmail.com>
2022-12-21 12:44:35 +0100

    Example008_Magic

--- Example008_Magic/Program.cs
+++ Example008_Magic/Program.cs
@@ -1,4 +1,5 @@
 ﻿Console.Clear();
+
 //Console.SetCursorPosition (10, 4);
 //Console.WriteLine("+");
 
@@ -7,9 +8,9 @@
 //int xb = 1;
 //int yb = 30; либо можно записать перечисление так:
 
-int x1 = 2, y1 = 2,
-    x2 = 2, y2 = 30,
-    x3 = 40, y3 = 30;
+int x1 = 1, y1 = 1,
+    x2 = 1, y2 = 30,
+    x3 = 80, y3 = 30;
 
 Console.SetCursorPosition(x1, y1);
 Console.WriteLine("+");
@@ -19,3 +20,32 @@ Console.WriteLine("+");
 
 Console.SetCursorPosition(x3, y3);
 Console.WriteLine("+");
+
+int x = x1, y = x2;
+
+int count = 0;
+
+while (count < 100)
+{
+    int what = new Random().Next(0, 3);
+    if (what == 0)
+    {
+        x = (x + x1) / 2;
+        y = (y + y1) / 2;
+    }
+
+    if (what == 1)
+    {
+        x = (x + x2) / 2;
+        y = (y + y2) / 2;
+    }
+
+    if (what == 2)
+    {
+        x = (x + x3) / 2;
+        y = (y + y3) / 2;
+    }
+    Console.SetCursorPosition(x, y);
+    Console.WriteLine("+");
+    count = count + 1; // либо можно использовать такие варианты записи (при увеличении на 1): count +=1; или count++;
+}
