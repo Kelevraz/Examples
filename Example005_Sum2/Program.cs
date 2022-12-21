1c11df116d7db6a0d51509c2b33ab06e59a9d395
--replace-all <zinovskayak@gmail.com>
2022-12-21 11:20:12 +0100

    Example005_Sum2 - add Random

---
+++ Example005_Sum2/Program.cs
@@ -0,0 +1,11 @@
+﻿// вывод суммы двух чисел
+//numberA - переменная
+
+int numberA = new Random().Next(1,10); // 1 2 3 4 .... 9
+Console.WriteLine(numberA);
+int numberB = new Random().Next(1,10);
+Console.WriteLine(numberB);
+//можно сделать с промежуточной переменной:
+//int result = numberA + numberB;
+//Console.WriteLine(result);
+Console.WriteLine(numberA + numberB);
