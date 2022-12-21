4b92f464b2ce1316518e672e7c08332db2ec25fe
--replace-all <zinovskayak@gmail.com>
2022-12-21 11:36:47 +0100

    Example006_ifElse

---
+++ Example006_ifElse/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Введите имя пользователя:");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "катя")
+{
+    Console.WriteLine("Ты охуенна!");
+}
+else
+{
+    Console.Write("Привет, ");
+    Console.WriteLine(username);
+}
