84aac6d70e22b9780ff8e6f9faeaf60b5e9c3912
--replace-all <zinovskayak@gmail.com>
2022-12-21 12:19:31 +0100

    Example008_Magic

---
+++ Example008_Magic/Program.cs
@@ -0,0 +1,21 @@
+﻿Console.Clear();
+//Console.SetCursorPosition (10, 4);
+//Console.WriteLine("+");
+
+//int xa = 1;
+//int ya = 1;
+//int xb = 1;
+//int yb = 30; либо можно записать перечисление так:
+
+int x1 = 2, y1 = 2,
+    x2 = 2, y2 = 30,
+    x3 = 40, y3 = 30;
+
+Console.SetCursorPosition(x1, y1);
+Console.WriteLine("+");
+
+Console.SetCursorPosition(x2, y2);
+Console.WriteLine("+");
+
+Console.SetCursorPosition(x3, y3);
+Console.WriteLine("+");
