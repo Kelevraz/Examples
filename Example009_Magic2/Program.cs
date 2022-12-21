88db8ae1beea3f3b08a7fe7bbe8d974d8945e23b
--replace-all <zinovskayak@gmail.com>
2022-12-21 15:23:51 +0100

    устранили ошибки

--- Example009_Magic2/Program.cs
+++ Example009_Magic2/Program.cs
@@ -1,19 +1,7 @@
 ﻿Console.WriteLine("Привет!");
 Console.WriteLine("Что я должен сделать?");
-string username = Console.ReadLine();
-
-if(username.ToLower() == "ты знаешь")
-{
-    Console.WriteLine("Понял ;) подтверди отправку");
-    string ans = Console.ReadLine();
-    if(ans.ToLower() == "yes")
-    {
-    Console.WriteLine("КАТЯ!");
-    Console.WriteLine("ТЫ ОХУЕННА!");
-    }
-}
-else
-{
-    Console.Write("Привет, ");
-    Console.WriteLine(username);
-}
+Console.ReadLine();
+Console.WriteLine("Понял, подтверди отправку");
+Console.ReadLine();
+Console.WriteLine("КАТЯ!");
+Console.WriteLine("Ты охуенна!");
