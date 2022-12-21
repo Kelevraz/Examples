45e60815705448fea00c07969e71d53d4ebf2817
--replace-all <zinovskayak@gmail.com>
2022-12-21 15:08:03 +0100

    Example009_Magic2

---
+++ Example009_Magic2/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.WriteLine("Привет!");
+Console.WriteLine("Что я должен сделать?");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "ты знаешь")
+{
+    Console.WriteLine("Понял ;) подтверди отправку");
+    string ans = Console.ReadLine();
+    if(ans.ToLower() == "yes")
+    {
+    Console.WriteLine("КАТЯ!");
+    Console.WriteLine("ТЫ ОХУЕННА!");
+    }
+}
+else
+{
+    Console.Write("Привет, ");
+    Console.WriteLine(username);
+}
