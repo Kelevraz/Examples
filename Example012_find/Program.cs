7ca1c269cd0d091b4f9b8a6cc47add9bc85d8dd6
--replace-all <zinovskayak@gmail.com>
2022-12-22 14:26:16 +0100

    решаем задачки с массивами

---
+++ Example012_find/Program.cs
@@ -0,0 +1,15 @@
+﻿int[] array = { 11, 32, 23, 44, 5, 643, 78, 81, 19 };
+
+int n = array.Length;
+int find = 4;
+int index = 0;
+
+while (index < n)
+{
+    if(array[index] == find)
+    {
+        Console.WriteLine(index);
+    }
+    index = index + 1;
+    // index++;
+}
